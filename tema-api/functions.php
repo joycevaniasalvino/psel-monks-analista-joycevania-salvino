652aae661de09753585a479cd9f5b3cd77a72f30
Joycevânia Salvino <joycelima773@gmail.com>
2025-04-21 23:54:28 -0300

    :arrow_up: update: atualizando api

--- tema-api/functions.php
+++ tema-api/functions.php
@@ -1,6 +1,14 @@
 <?php
 
 add_action('rest_api_init', function () {
+    remove_filter('rest_pre_serve_request', 'rest_send_cors_headers');
+    add_filter('rest_pre_serve_request', function ($value) {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type');
+        return $value;
+    });
+
     register_rest_route('minhaapi/v1', '/dados/', [
         'methods' => 'GET',
         'callback' => 'minha_api_callback',
@@ -9,10 +17,12 @@ add_action('rest_api_init', function () {
 
 function minha_api_callback()
 {
+    $theme_uri = get_template_directory_uri();
+
     return [
         [
             'id' => 1,
-            'imgLogo' => 'link',
+            'imgLogo' => $theme_uri . '/assets/imagens/logo.png',
             'linksHeader' => [
                 [
                     'id' => 1,
@@ -37,67 +47,50 @@ function minha_api_callback()
             ],
             'tituloPrincipal' => 'Lorem ipsum dolor sit amet consectetur',
             'textoprincipal' => 'Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo aenean viverra',
-            'imgPrincipal' => 'link',
-            'imgScroll'=> 'link'
-        ],
-        [
-            'id' => 2,
+            'imgPrincipal' => $theme_uri . '/assets/imagens/logoSecao.png',
+            'imgScroll' => $theme_uri . '/assets/imagens/Scroll.png',
+
             'tituloSecao2' => 'Lorem ipsum dolor sit amet consectetur',
             'textoSecao2' => 'Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo',
             'cards' => [
                 [
                     'id' => 1,
-                    'imgCard' => 'link',
-                    'tituloCard' => 'Lorem ipsum dolorLorem ipsum dolor',
+                    'imgCard' => $theme_uri . '/assets/imagens/imgCard.jpg',
+                    'tituloCard' => 'Lorem ipsum dolor',
                     'textCard' => 'Lorem ipsum dolor sit amet consectetur.'
                 ],
                 [
                     'id' => 2,
-                    'imgCard' => 'link',
-                    'tituloCard' => 'Lorem ipsum dolorLorem ipsum dolor',
+                    'imgCard' => $theme_uri . '/assets/imagens/imgCard2.jpg',
+                    'tituloCard' => 'Lorem ipsum dolor',
                     'textCard' => 'Lorem ipsum dolor sit amet consectetur.'
                 ],
                 [
                     'id' => 3,
-                    'imgCard' => 'link',
-                    'tituloCard' => 'Lorem ipsum dolorLorem ipsum dolor',
+                    'imgCard' => $theme_uri . '/assets/imagens/imgCard3.jpg',
+                    'tituloCard' => 'Lorem ipsum dolor',
                     'textCard' => 'Lorem ipsum dolor sit amet consectetur.'
                 ],
                 [
                     'id' => 4,
-                    'imgCard' => 'link',
-                    'tituloCard' => 'Lorem ipsum dolorLorem ipsum dolor',
+                    'imgCard' => $theme_uri . '/assets/imagens/imgCard4.jpg',
+                    'tituloCard' => 'Lorem ipsum dolor',
                     'textCard' => 'Lorem ipsum dolor sit amet consectetur.'
                 ]
-            ]
-        ],
-        [
-            'id' => 3,
+            ],
             'tituloSecao3' => 'Lorem ipsum dolor sit amet consectetur',
             'textoSecao3' => 'Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo',
-            'imagesSecao3' => [
-                [
-                    'img1' => 'link'
-                ],
-                [
-                    'img2' => 'link'
-                ],
-                [
-                    'img3' => 'link'
-                ]
-            ]
-        ],
-        [
-            'id' => 4,
+            'img1' => $theme_uri . '/assets/imagens/imgsecao3.png',
+            'img2' => $theme_uri . '/assets/imagens/imgsecao32.png',
+            'img3' => $theme_uri . '/assets/imagens/imgsecao33.png',
+
             'tituloSecao4' => 'Lorem ipsum dolor sit amet consectetur',
             'textSecao4' => 'Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo aenean viverra',
-            'imgAppleStore' => 'link',
-            'imgPlayStore' => 'link',
-        ],
-        [
-            'id' => 5,
+            'imgAppleStore' => $theme_uri . '/assets/imagens/imgPS.png',
+            'imgPlayStore' => $theme_uri . '/assets/imagens/imgAS.png',
+
             'tituloSecao5' => 'Lorem ipsum dolor sit amet consectetur',
-            'links' => [
+            'tags' => [
                 [
                     'id' => 1,
                     'titulo' => 'Perfumaria',
@@ -173,47 +166,45 @@ function minha_api_callback()
                     'titulo' => 'Proteção solar',
                     'link' => 'link'
                 ],
-            ]
-        ],
-        [
-            'id' => 6,
-            'cards' => [
+            ],
+            'cards2' => [
                 [
                     'id' => 1,
-                    'titulo' => 'Lorem ipsum dolor sit amet',
-                    'texto' => 'Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo aenean viverra.'
+                    'tituloCard' => 'Lorem ipsum dolor sit amet',
+                    'textCard' => 'Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo aenean viverra.'
                 ],
                 [
                     'id' => 2,
-                    'titulo' => 'Lorem ipsum dolor sit amet',
-                    'texto' => 'Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo aenean viverra.'
+                    'tituloCard' => 'Lorem ipsum dolor sit amet',
+                    'textCard' => 'Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo aenean viverra.'
                 ],
                 [
                     'id' => 3,
-                    'titulo' => 'Lorem ipsum dolor sit amet',
-                    'texto' => 'Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo aenean viverra.'
+                    'tituloCard' => 'Lorem ipsum dolor sit amet',
+                    'textCard' => 'Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo aenean viverra.'
                 ],
-            ]
-        ],
-        [
-            'id' => 7,
+                [
+                    'id' => 4,
+                    'tituloCard' => 'Lorem ipsum dolor sit amet',
+                    'textCard' => 'Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo aenean viverra.'
+                ]
+            ],
+            'imgForm' => $theme_uri . '/assets/imagens/imgForm.png',
             'tituloForm' => 'Lorem ipsum dolor sit amet consectetur',
             'textoForm' => 'Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque',
-            'textoFormDetalhe'=> '*Lorem ipsum dolor sit amet consectetur'
-        ],
-        [   
-            'id' => 8,
-            'instagram' => 'link',
-            'whatsapp' => 'link',
-            'x' => 'link',
-            'facebook' => 'link',
+            'textoFormDetalhe' => '*Lorem ipsum dolor sit amet consectetur',
+
+            'instagram' => '#',
+            'whatsapp' => '#',
+            'twitter' => '#',
+            'facebook' => '#',
 
             'fraseRodape' => 'Lorem ipsum dolor sit amet',
-            'link1' => 'link',
-            'link2' => 'link',
-            'link3' => 'link',
-            'link4' => 'link'
+            'link1' => '#',
+            'link2' => '#',
+            'link3' => '#',
+            'link4' => '#'
+
         ],
     ];
 }
-
